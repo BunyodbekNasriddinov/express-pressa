5f60a31ddfa952856660d3f6e3d714923f7f4747
BunyodbekNasriddinov <nasriddinoffbunydobek@gmail.com>
2023-05-29 21:58:48 +0500

    Type change ES module

--- src/middlewares/checkToken.js
+++ src/middlewares/checkToken.js
@@ -1,16 +1,17 @@
-const { verify } = require("jsonwebtoken");
+import jwt from "jsonwebtoken";
+import { AuthorizationError } from "../utils/errors";
 
 module.exports = function (req, res, next) {
-	const { token } = req.headers;
-	try {
-		if (!token) {
-			throw new Error("Token required");
-		}
+  const { token } = req.headers;
+  try {
+    if (!token) {
+      next(new AuthorizationError("Token required"));
+    }
 
-		if (verify(token, process.env.SECRET_KEY)) {
-			next();
-		}
-	} catch (error) {
-		res.status(401).send({ status: 401, message: error.message });
-	}
+    if (jwt.verify(token, process.env.SECRET_KEY)) {
+      next();
+    }
+  } catch (error) {
+    res.status(401).send({ status: 401, message: error.message });
+  }
 };
--- src/routes/admin.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const admin = require("../controllers/admin.controller");
-
-const router = express.Router();
-
-router.post("/", admin.POST);
-
-module.exports = router;
